bb742e8599361edf963887f8581eb51924df8eb8
victornietol <nietovirgo17@gmail.com>
2025-11-26 01:07:02 -0600

    Se implemento la busqueda de inventario por unidades unicas y se corrigieron las busquedas por campos numericos

--- src/main/java/org/victornieto/gestionbiblioteca/repository/LibroRepositoryImp.java
+++ src/main/java/org/victornieto/gestionbiblioteca/repository/LibroRepositoryImp.java
@@ -10,9 +10,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LibroRepositoryImp implements LibroRepository {
+
+    private final Set<String> numericColumns = Set.of("lib.id_libro", "lib.anio_publicacion", "lib.paginas", "uni.unidades", "ej.id", "libro.anio_publicacion", "libro.paginas");
 
     public LibroRepositoryImp() {
     }
@@ -48,7 +52,13 @@ public class LibroRepositoryImp implements LibroRepository {
 
             if (!columnToSearch.equals("todos")) {
                 // Se aplica clausula WHERE
-                stmt.setString(1, coincidenceToSearch);
+                if (numericColumns.contains(columnToSearch)) {
+                    // si es numerico
+                    stmt.setInt(1, Integer.parseInt(coincidenceToSearch));
+                } else {
+                    stmt.setString(1, coincidenceToSearch);
+                }
+
             }
 
             ResultSet resultSet = stmt.executeQuery();
@@ -139,16 +149,139 @@ public class LibroRepositoryImp implements LibroRepository {
                         ) AS uni ON (lib.id_libro = uni.id_libro)
         """;
 
-        String queryUnits = "";
+        String queryUnits = """
+                SELECT
+                	ej.id AS id_libro,
+                	libro.titulo AS titulo,
+                	libro.autores  AS autor,
+                	libro.categoria AS categoria,
+                	libro.editorial AS editorial,
+                	libro.anio_publicacion as anio_publicacion,
+                	libro.paginas AS paginas,
+                	libro.edicion AS edicion,
+                    1 AS unidades
+                FROM (
+                	SELECT
+                		id,
+                		id_libro
+                	FROM ejemplar_libro
+                	WHERE activo = 1
+                ) AS ej
+                JOIN (
+                	SELECT
+                		lib.id_libro AS id_libro,
+                		lib.titulo AS titulo,
+                		lib.autores  AS autores,
+                		cat.categoria AS categoria,
+                		lib.editorial AS editorial,
+                		lib.anio_publicacion as anio_publicacion,
+                		lib.paginas AS paginas,
+                		lib.edicion AS edicion
+                	FROM (
+                		SELECT
+                			l.id AS id_libro,
+                			l.titulo AS titulo,
+                			GROUP_CONCAT(CONCAT(a.nombre, ' ', a.apellido_p, ' ', a.apellido_m) SEPARATOR ', ') AS autores,
+                			ed.nombre AS editorial,
+                			l.anio_publicacion as anio_publicacion,
+                			l.no_paginas AS paginas,
+                			l.edicion AS edicion
+                		FROM libro_autor l_a
+                		JOIN autor a ON (l_a.id_autor = a.id)
+                		JOIN libro l ON (l.id = l_a.id_libro)
+                		JOIN editorial ed ON (l.id_editorial=ed.id)
+                		WHERE l.activo = 1
+                		GROUP BY l.id) AS lib
+                	JOIN (
+                		SELECT
+                			l.id AS id_libro,
+                			GROUP_CONCAT(c.nombre SEPARATOR ', ') AS categoria
+                		FROM categoria_libro c_l
+                		JOIN libro l ON (c_l.id_libro = l.id)
+                		JOIN categoria c ON (c_l.id_categoria = c.id)
+                		WHERE l.activo = 1
+                		GROUP BY l.id
+                	) AS cat ON (lib.id_libro = cat.id_libro)
+                ) AS libro ON (ej.id_libro = libro.id_libro)
+                """;
 
-        String where = " WHERE " + columnToSearch + " LIKE CONCAT('%', ?, '%') ";
         String orderBy = "ORDER BY " + orderByColumn + " " + (orderDesc ? "DESC" : "ASC");
 
         String query = showTitles ? queryTitles : queryUnits;
 
         if (columnToSearch.equals("todos")) {
             query += orderBy;
+
         } else {
+            // Implementar clasula WHERE
+            String where;
+
+            if (showTitles) { // Dependiendo de la consulta
+                switch (columnToSearch) {
+                    case "id_libro":
+                        columnToSearch = "lib.id_libro";
+                        break;
+                    case "titulo":
+                        columnToSearch = "lib.titulo";
+                        break;
+                    case "autor":
+                        columnToSearch = "lib.autores";
+                        break;
+                    case "categoria":
+                        columnToSearch = "cat.categoria";
+                        break;
+                    case "editorial":
+                        columnToSearch = "lib.editorial";
+                        break;
+                    case "anio_publicacion":
+                        columnToSearch = "lib.anio_publicacion";
+                        break;
+                    case "paginas":
+                        columnToSearch = "lib.paginas";
+                        break;
+                    case "edicion":
+                        columnToSearch = "lib.edicion";
+                        break;
+                    case "unidades":
+                        columnToSearch = "uni.unidades";
+                        break;
+                }
+            } else {
+                switch (columnToSearch) {
+                    case "id_libro":
+                        columnToSearch = "ej.id";
+                        break;
+                    case "titulo":
+                        columnToSearch = "libro.titulo";
+                        break;
+                    case "autor":
+                        columnToSearch = "libro.autores";
+                        break;
+                    case "categoria":
+                        columnToSearch = "libro.categoria";
+                        break;
+                    case "editorial":
+                        columnToSearch = "libro.editorial";
+                        break;
+                    case "anio_publicacion":
+                        columnToSearch = "libro.anio_publicacion";
+                        break;
+                    case "paginas":
+                        columnToSearch = "libro.paginas";
+                        break;
+                    case "edicion":
+                        columnToSearch = "libro.edicion";
+                        break;
+                    default:
+                        //
+                }
+            }
+
+            if (numericColumns.contains(columnToSearch)) {
+                where = " WHERE " + columnToSearch + " = ? "; // campo numerico
+            } else {
+                where = " WHERE " + columnToSearch + " LIKE CONCAT('%', ?, '%') "; // campo no numerico
+            }
             query = query + where + orderBy;
         }
 
